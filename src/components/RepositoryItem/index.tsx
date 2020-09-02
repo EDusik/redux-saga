1010f97dea186c4b91f2c3c450dada13149f1e31
EDusik <eduardodusik@gmail.com>
2020-09-01 21:10:21 -0300

    :tada:

---
+++ src/components/RepositoryItem/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Repository } from '../../store/ducks/repositories/types';
+
+interface OwnProps {
+  repository: Repository
+}
+
+export default function RepositoryItem({ repository }: OwnProps) {
+
+  return <li>{repository.name}</li>;
+}
